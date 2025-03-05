5d03b1ee6d030570574539d0d8f07eeb926e4f8b
DanielZR-web <dzamoruiz@gmail.com>
2025-03-05 20:35:50 +0100

    Fix render HTML

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, request, jsonify, render_template
 import ipaddress
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder="static", template_folder="templates")
+app = Flask(__name__)
 CORS(app)  # Permitir peticiones desde cualquier origen
 
 @app.route("/")
-def index():
-    return render_template("index.html")  # Renderiza el HTML al entrar a la URL raíz
+def home():
+    return render_template("index.html")  # Asegúrate de que el archivo esté en /templates
 
+# Rutas para la API
 @app.route("/calcular_red", methods=["POST"])
 def calcular_red():
     data = request.json
@@ -24,36 +25,6 @@ def calcular_red():
         })
     except ValueError:
         return jsonify({"error": "Prefijo inválido."}), 400
-
-@app.route("/determinar_prefijo", methods=["POST"])
-def determinar_prefijo():
-    data = request.json
-    try:
-        num_hosts = int(data.get("num_hosts"))
-        for i in range(32, 0, -1):
-            red = ipaddress.IPv4Network(f'0.0.0.0/{i}', strict=False)
-            if red.num_addresses - 2 >= num_hosts:
-                return jsonify({
-                    "prefijo": i,
-                    "mascara": str(red.netmask)
-                })
-        return jsonify({"error": "Número de hosts demasiado grande."}), 400
-    except ValueError:
-        return jsonify({"error": "Número inválido."}), 400
-
-@app.route("/calcular_rango", methods=["POST"])
-def calcular_rango():
-    data = request.json
-    try:
-        direccion = data.get("direccion")
-        prefijo = int(data.get("prefijo"))
-        red = ipaddress.IPv4Network(f'{direccion}/{prefijo}', strict=False)
-        return jsonify({
-            "red_inicio": str(red.network_address),
-            "red_fin": str(red.broadcast_address)
-        })
-    except ValueError:
-        return jsonify({"error": "Dirección o prefijo inválido."}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)

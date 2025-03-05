3554f4733b267fba1dd470651e9735d8a2649b0d
DanielZR-web <dzamoruiz@gmail.com>
2025-03-05 08:53:21 +0100

    Corrección de archivos para Render

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def calcular_rango():
         return jsonify({"error": "Dirección o prefijo inválido."}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+     port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
